912229c160a6aad6ab0361034ddc42a460914770
evf <evf@windows>
2026-06-04 02:15:56 -0300

    Alias cd to $HOME

--- powershell/aliases.ps1
+++ powershell/aliases.ps1
@@ -1,13 +1,10 @@
-function .. {
-    Set-Location ..
-}
+function .. { Set-Location .. }
+function ~ { Set-Location ~ }
 Set-Alias touch New-Item
 Set-Alias rm Remove-Item
 Set-Alias mv Move-Item
 Set-Alias l eza
-function Get-DetailedList {
-    eza -lah
-}
+function Get-DetailedList { eza -lah }
 Set-Alias ll Get-DetailedList
 Set-Alias grep findstr
 Set-Alias gg lazygit
