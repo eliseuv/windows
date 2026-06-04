f0559322891c3e7d5b026b7175ef08c69374fb4c
evf <evf@windows>
2026-06-03 22:06:30 -0300

    eza aliases

--- powershell/aliases.ps1
+++ powershell/aliases.ps1
@@ -2,4 +2,9 @@ Set-Alias grep findstr
 Set-Alias touch New-Item
 Set-Alias rm Remove-Item
 Set-Alias mv Move-Item
+Set-Alias l eza
+function Get-DetailedList {
+    eza -lah
+}
+Set-Alias ll Get-DetailedList
 Set-Alias gg lazygit
