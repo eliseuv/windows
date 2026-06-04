16da28524abc6a6a5568e29340042bec9a984fcf
Eliseu Venites Filho <evenites@nelogica.com.br>
2026-06-03 23:04:26 -0300

    Alias cd to parent

--- powershell/aliases.ps1
+++ powershell/aliases.ps1
@@ -1,4 +1,6 @@
-Set-Alias grep findstr
+function .. {
+    Set-Location ..
+}
 Set-Alias touch New-Item
 Set-Alias rm Remove-Item
 Set-Alias mv Move-Item
@@ -7,4 +9,5 @@ function Get-DetailedList {
     eza -lah
 }
 Set-Alias ll Get-DetailedList
+Set-Alias grep findstr
 Set-Alias gg lazygit
